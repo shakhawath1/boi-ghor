4e93189833a4654b6a6e9339472675cf2b2dfeec
shakhawath <m.shakhawath18@gmail.com>
2022-03-26 22:01:51 +0600

    upeated responsiveness

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -10,13 +10,13 @@ const Question = () => {
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                How React works?
+                                How does React works?
                             </button>
                         </h2>
                         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                React is a library of JavaScript. React create virtual DOM of menipulate codes. React complied the code using Babel.
+                                React is a library of JavaScript. React create virtual DOM of menipulate codes.  This is the syntax of React components used to describe UI structures. React complied the code using Babel. This is data that comes from somewhere and is rendered by the component.
                             </div>
                         </div>
                     </div>
@@ -30,7 +30,7 @@ const Question = () => {
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                Props cannot be changed, only read. State will be changed. Props use for send data to child components.
+                                Props cannot be changed, only read. State will be changed. Props use for send data to child components. They should not be modified in any way. If you need to modify some value in response to user input or a network response, use state instead. Props changed by parent Component, but state is not changed by parent Component. Props should not change inside Component, state change inside Component.
                             </div>
                         </div>
                     </div>
@@ -38,13 +38,13 @@ const Question = () => {
                         <h2 class="accordion-header" id="headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                How useState works?
+                                How does useState works?
                             </button>
                         </h2>
                         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                Localstorage and sessionstorage are also similar, but there are some differences between
+                                Returns a stateful value, and a function to update it. During the initial render, the returned state is the same as the value passed as the first argument. The setState function is used to update the state. It accepts a new state value and enqueues a re-render of the component.
 
                             </div>
                         </div>
