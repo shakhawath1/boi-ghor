7aa31da781b65f0419659b183f27f5449751e44d
shakhawath <m.shakhawath18@gmail.com>
2022-06-06 15:17:16 +0600

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand fs-2 fw-bold" href="/">BOI GHOR</a>
+                <a className="navbar-brand fs-2 fw-bold me-2" href="/">BOI GHOR</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
